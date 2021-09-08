1da2b54581690decd3bdce70dcacfea4ff18355b
Giovanni  Rosales <giovannirosales@Giovannis-MacBook-Pro.local>
2021-09-07 19:39:34 -0500

    finished

--- index.js
+++ index.js
@@ -192,8 +192,8 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(kilometers){
+  return kilometers * 0.621371;
 }
 
 
@@ -206,8 +206,8 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm/30.48;
 }
 
 
@@ -222,8 +222,10 @@ Using the annoyingSong function below do the following:
     "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+function annoyingSong(number){
+   for(let i=number; i > 0; i--){
+     return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i -1} bottles of soda on the wall`;
+   }
 }
 
 
@@ -242,8 +244,18 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(number){
+  if (number >= 90){
+    return `you got an A`;
+  }else if (number <90 && number >=80){
+    return `you got a B`;
+  }else if (number < 80 && number >= 70){
+    return `you got a C`;
+  }else if (number < 70 && number >= 60){
+    return `you got a D`;
+  }else{
+    return `you got an F`;
+  }
 }
 
 
